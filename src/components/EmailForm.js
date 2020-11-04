1bb89d04ff90e0352d174dedbecbe8b14e87d1ff
User <user@Users-iMac.local>
2020-11-04 13:59:17 +0200

    half-working vaidation, before putting terms into form

--- src/components/EmailForm.js
+++ src/components/EmailForm.js
@@ -7,30 +7,84 @@ import './ErrorMessage.sass'
 import emailTldExtractor from './../utils/emailTldExtractor';
 import SocialApps from './SocialApps';
 
-const probihidetTlds = {
+const prohibitedTlds = {
     "country": "Columbia",
     "topLevelDomain": "co"
 };
 
 function EmailForm(props) {
-    const [errorMessage, setErrorMessage] = useState('');
-    const userEmailInput = React.createRef();
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        console.log(userEmailInput.current.value);
+    const [formState, setFormState] = useState({
+        email: '',
+        terms: false,
+        errorMessage: ''
+    });
+    // const userEmailInput = React.createRef();
+    function handleChange(event) {
+        const value =
+            event.target.type === 'checkbox' ?
+                event.target.checked : event.target.value;
+        setFormState({
+            ...formState,
+            [event.target.name]: value
+        });
+        console.log('handleChange: ', formState);
+    };.
 
-        if (emailTldExtractor(userEmailInput.current.value) === '') {
-            setErrorMessage(`Please provide a valid e-mail address.`);
+    function handleSubmit(event) {
+        event.preventDefault();
+
+
+        if (formState.email === '') {
+            setFormState({
+                ...formState,
+                errorMessage: 'Please provide a valid e-mail address.'
+            });
+        } else if (formState.email === prohibitedTlds.topLevelDomain) {
+            setFormState({
+                ...formState,
+                errorMessage: 'We are not accepting subscriptions from ' + prohibitedTlds.country + ' emails.'
+            });
+        } else if (formState.terms == false) {
+            setFormState({
+                ...formState,
+                errorMessage: 'You must accept the terms and conditions.'
+            });
         } else {
-            if (emailTldExtractor(userEmailInput.current.value) === probihidetTlds.topLevelDomain) {
-                setErrorMessage(`We are not accepting subscriptions from ${probihidetTlds.country} emails.`);
-                console.log({ errorMessage });
-            } else {
-                setErrorMessage("");
-                props.stateChanger();
-            };
-        }
-    };
+            setFormState({
+                ...formState,
+                errorMessage: ''
+            });
+            props.stateChanger();
+        };
+
+        // if (formState.terms === false) {
+        //     setFormState(formState.errorMessage = 'You must accept the terms and conditions.');
+
+        // } else if (formState.email === '') {
+        //     setFormState(formState.errorMessage = 'Please provide a valid e-mail address.');
+        // } else if (formState.email === prohibitedTlds.topLevelDomain) {
+        //     setFormState(formState.errorMessage = 'We are not accepting subscriptions from ' + prohibitedTlds.country + ' emails.');
+        // } else {
+        //     setFormState(formState.errorMessage = '');
+        //     props.stateChanger();
+        // }
+
+        // if (emailTldExtractor(formState.email) === '') {
+        //     setFormState(formState.errorMessage = 'Please provide a valid e-mail address.');
+        // } else {
+        //     if (emailTldExtractor(formState.email) === prohibitedTlds.topLevelDomain) {
+        //         setFormState(formState.errorMessage = 'We are not accepting subscriptions from ' + prohibitedTlds.country + ' emails.');
+        //         console.log( formState.errorMessage );
+        //     } else {
+        //         setFormState(formState.errorMessage = "");
+        //         props.stateChanger();
+        //     };
+        // }
+
+        console.log('handleSubmit: ',formState);
+        alert('email: ' + formState.email + ', terms: ' + formState.terms + ', errorMessage: ' + formState.errorMessage);
+    }
+
 
     return (
         <div className='form'>
@@ -39,13 +93,17 @@ function EmailForm(props) {
                 <p>Subscribe to our newsletter and get 10% discount on pineapple glasses.</p>
             </div>
             <div className='prevent-growin-by-child'>
-                <form onSubmit={handleSubmit} className='email-box'>
+                <form
+                    onSubmit={handleSubmit} className='email-box'
+                >
                     <input
                         type="email"
+                        name='email'
+                        vaue={formState.email}
+                        onChange={handleChange}
                         className='email'
                         size='20'
                         placeholder="Type your email address here..."
-                        ref={userEmailInput}
                         required
                     />
                     <input type="image" id="image" alt="Submit"
@@ -54,16 +112,23 @@ function EmailForm(props) {
                     />
                 </form>
             </div>
-            {errorMessage && <ErrorMessage message={errorMessage} />}
             <div className='terms'>
                 <label className="terms-container">I agree to <a href="#t">terms of service</a>
-                    <input type="checkbox" />
+                    <input
+                        type="checkbox"
+                        name='terms'
+                        checked={formState.terms}
+                        onChange={handleChange} />
                     <span className="checkmark"></span>
                 </label>
             </div>
+            {formState.errorMessage && <ErrorMessage message={formState.errorMessage} />}
+
             <SocialApps />
         </div>
     );
 };
 
 export default EmailForm;
+
+//ref={userEmailInput}
